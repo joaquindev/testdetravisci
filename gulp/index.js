97c00ef8235bafb9c71015e74c457410d1cce789
joaquindev <joaquindavid@gmail.com>
2017-02-21 20:18:52 +0100

    Solved error

--- gulp/index.js
+++ gulp/index.js
@@ -2,7 +2,7 @@
 var fs = require('fs');
 var tasks = fs.readdirSync('./gulp/tasks');
 var gulp = require('gulp');
-tasks.forEach((task)=>{
+tasks.forEach(function(task){
 	require('./tasks/' + task);
 });
 
